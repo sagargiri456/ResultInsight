a830dbd776b195be60ed9106b4367696b21182e7
sagargiri456 <sagargiri456@gmail.com>
2025-02-16 23:01:31 +0530

    added export const runtime at the top of the route.ts

--- app/api/marksheets/route.ts
+++ app/api/marksheets/route.ts
@@ -1,13 +1,16 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic"; // Ensure the API is always dynamically executed
+//just changed the code a little bit;
+
+
 import { NextResponse } from "next/server";
 import prisma  from "@/lib/db";
 
-export const dynamic = "force-dynamic"; // Ensure the API is always dynamically executed
-export const runtime = "nodejs";
 
 export async function GET() {
   try {
     const marksheets = await prisma.marksheet.findMany();
-    return NextResponse.json({ urls: marksheets.map((m) => m.url) });
+    return NextResponse.json({ urls: marksheets.map((p) => p.url) });
   } catch (error) {
     return NextResponse.json({ error: error instanceof Error ? error.message : "Unknown error" },
       { status: 500 });
